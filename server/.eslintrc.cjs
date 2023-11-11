10f4498952c9251f80161b9be3afb07901cda657
Utavegu <utavegu@list.ru>
2023-11-11 20:28:51 +0500

    build: add and setup editorconfig, eslint, stylelint and prettier

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+  },
+};
